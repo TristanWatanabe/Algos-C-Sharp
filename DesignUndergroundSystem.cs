8cbf3d7316e54e0ace4dd694ca61f2c7624c6076
TristanWatanabe <tristan.watanabe@gmail.com>
2021-01-05 01:10:36 -0500

    Solution Added

--- DesignUndergroundSystem.cs
+++ DesignUndergroundSystem.cs
@@ -33,6 +33,73 @@ namespace Algos_C_Sharp
 {
     class DesignUndergroundSystem
     {
+        public class UndergroundSystem
+        {
 
+            public Dictionary<int, CheckIn> CheckIns = new Dictionary<int, CheckIn>();
+            public Dictionary<string, Journey> Journeys = new Dictionary<string, Journey>();
+
+            public UndergroundSystem() { }
+
+            public void CheckIn(int id, string stationName, int t)
+            {
+                var checkIn = new CheckIn(stationName, t);
+                CheckIns.Add(id, checkIn);
+            }
+
+            public void CheckOut(int id, string stationName, int t)
+            {
+                var checkIn = CheckIns[id];
+                int timeDiff = t - checkIn.Time;
+                string key = $"{checkIn.StationName} - {stationName}";
+                if (Journeys.ContainsKey(key))
+                {
+                    var journey = Journeys[key];
+                    int updatedTotalTime = journey.TotalTime + timeDiff;
+                    int updatedNumOfTrips = journey.NumOfTrips + 1;
+                    double updatedAverageTime = (double)updatedTotalTime / updatedNumOfTrips;
+                    journey.TotalTime = updatedTotalTime;
+                    journey.NumOfTrips = updatedNumOfTrips;
+                    journey.AverageTime = updatedAverageTime;
+                }
+                else
+                {
+                    var newJourney = new Journey(timeDiff, 1, timeDiff);
+                    Journeys.Add(key, newJourney);
+                }
+                CheckIns.Remove(id);
+            }
+
+            public double GetAverageTime(string startStation, string endStation)
+            {
+                string key = $"{startStation} - {endStation}";
+                return Journeys[key].AverageTime;
+            }
+        }
+
+        public class CheckIn
+        {
+            public string StationName { get; }
+            public int Time { get; }
+            public CheckIn(string stationName, int time)
+            {
+                StationName = stationName;
+                Time = time;
+            }
+        }
+
+        public class Journey
+        {
+            public int TotalTime { get; set; }
+            public int NumOfTrips { get; set; }
+            public double AverageTime { get; set; }
+
+            public Journey(int totalTime, int numOfTrips, double averageTime)
+            {
+                TotalTime = totalTime;
+                NumOfTrips = numOfTrips;
+                AverageTime = averageTime;
+            }
+        }
     }
 }
